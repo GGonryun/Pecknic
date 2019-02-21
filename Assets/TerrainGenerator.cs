f4ad21c9a5bf9b4d0ed4d406ec57116bdacb1412
Miguel Campos <amodestduck@gmail.com>
2019-02-21 14:27:23 -0800

    adding perlin noise

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -1,63 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private int xSize;
     [SerializeField] private int zSize;
-    [SerializeField] private TextMeshProUGUI pointPrefab;
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
-    private Mesh mesh;
-
 
     protected virtual void Awake()
     {
         meshFilter = gameObject.AddComponent<MeshFilter>();
         meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        mesh = new Mesh();
-        GenerateMesh();
+        meshFilter.mesh = GenerateMesh();
     }
 
-    //protected virtual void OnDrawGizmos()
-    //{
-    //    if(vertices != null)
-    //    {
-    //        for(int i = 0; i < vertices.Length; i++)
-    //        {
-    //            Gizmos.color = Color.black;
-    //            Gizmos.DrawSphere(vertices[i], .20f);
-    //        }
-    //    }
-    //}
-
-    private void SetPoints(Vector3[] points)
+    private Mesh GenerateMesh()
     {
-        for (int i = 0; i < points.Length; i++)
+        int totalVertices = (xSize + 1) * (zSize + 1);
+        Mesh mesh = new Mesh();
+        Vector2[] uv = new Vector2[totalVertices];
+        Vector3[] vertices = new Vector3[totalVertices];
+        Vector4[] tangents = new Vector4[totalVertices];
+        Vector4 tangent = new Vector4(1, 0, 0, -1);
+        for (int i = 0, z = 0; z <= xSize; z++)
         {
-            Vector3 point = points[i];
-            TextMeshProUGUI sticker = Instantiate(pointPrefab, point, Quaternion.identity, this.transform) as TextMeshProUGUI;
-            sticker.text = $"({point.x}, {point.y}, {point.z})";
-        }
-    }
-
-    private void GenerateMesh()
-    {
-        int size = (xSize + 1) * (zSize + 1);
-        Vector3[] vertices = new Vector3[size];
-        int[] triangles = new int[size * 6];
-
-        for (int i = 0, x = 0; x < xSize; x++)
-        {
-            for(int z = 0; z < zSize; z++, i++)
+            for (int x = 0; x <= zSize; x++, i++)
             {
                 int y = 0;
                 vertices[i] = new Vector3(x, y, z);
+
+                uv[i] = new Vector2((float)x / xSize, (float)z / zSize);
+
+                tangents[i] = tangent;
             }
         }
-        SetPoints(vertices);
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+        mesh.triangles = GenerateTriangles(totalVertices);
+        mesh.RecalculateNormals();
+        return mesh;
     }
 
+    private int[] GenerateTriangles(int totalVertices)
+    {
+        int[] triangles = new int[totalVertices * 6];
+        int nextRow = xSize;
+        for (int t = 0, v = 0, y = 0; y < zSize; y++, v++)
+        {
+            for (int x = 0; x < xSize; t += 6, v++, x++)
+            {
+                triangles[t] = v;
+                triangles[t + 2] = triangles[t + 3] = v + 1;
+                triangles[t + 1] = triangles[t + 4] = v + nextRow + 1;
+                triangles[t + 5] = v + nextRow + 2;
+            }
+        }
+        return triangles;
+    }
 }
